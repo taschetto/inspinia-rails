10ae9c2338dece125929bc8daffb9f9ceed4664e
Guilherme Taschetto <gtaschetto@gmail.com>
2014-11-11 00:52:28 -0200

    Layout refactoring.

--- vendor/assets/javascripts/inspinia/inspinia.js
+++ vendor/assets/javascripts/inspinia/inspinia.js
@@ -34,9 +34,9 @@ $(document).ready(function () {
     });
 
     // Append config box / Only for demo purpose
-    $.get("skin-config.html", function (data) {
-        $('body').append(data);
-    });
+    //$.get("skin-config.html", function (data) {
+    //    $('body').append(data);
+    //});
 
     // minimalize menu
     $('.navbar-minimalize').click(function () {
