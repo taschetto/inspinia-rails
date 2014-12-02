8c2c669a07aa58a6a7e940afed9b8d03978fd9b8
Guilherme Taschetto <gtaschetto@gmail.com>
2014-12-02 00:32:28 -0200

    Add validation to TimeEntry model.

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,4 +1,9 @@
 class TimeEntry < ActiveRecord::Base
   belongs_to :user
   belongs_to :task
+
+  validates :user_id, presence: true
+  validates :task_id, presence: true
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
 end
